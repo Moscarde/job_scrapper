bbd40b0836b73287a35e051ad1adf7051cf38a78
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-03-22 17:50:25 -0300

    feat: integrate all method modules into app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-import requests
+
 from endpoints import urls
-]
-print(urls)
+from datetime import datetime
+from methods.utils import Utils
+
+#Configuração do Ambiente / Criação de Pastas
+path = "lake"
+year = datetime.now().year
+month = datetime.now().month
+day = datetime.now().day
+
+utils = Utils()
+
+utils.createDir(path)
+utils.createDir(f"{path}/{year}")
+utils.createDir(f"{path}/{year}/{month}")
+utils.createDir(f"{path}/{year}/{month}/{day}")
+
+
