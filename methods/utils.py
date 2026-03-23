9767d1d0b38d68df3beb4fb643e7441d0a4c690c
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-03-22 23:25:43 -0300

    refactor(utils): add listDir and loadFile helper functions

--- methods/utils.py
+++ methods/utils.py
@@ -1,9 +1,16 @@
 import os
 
 class Utils:
-    def __init__():
+    def __init__(self):
         pass
     
-    def createDir(directory):
+    def createDir(self, directory):
         if not os.path.exists(directory):
             os.mkdir(directory)
+            
+    def listDir(self, directory):
+        return os.listdir(directory)
+    
+    def loadFile(self, file_name_path):
+        with open(file_name_path, "r") as f:
+            return f.read()
