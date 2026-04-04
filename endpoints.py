cfddd95269401229db291d110faaf14280569883
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-04-04 12:49:00 -0300

    refactor(config): centraliza queries de busca em endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -1,7 +1,29 @@
-urls = {
-    "remotar": {"url_q" : "https://remotar.com.br/search/jobs?q=", "active":0},
-    "weworkremotely": {"url_q": "https://weworkremotely.com/remote-jobs/search?term=", "active":0}, #status code 403
-    "skipthedrive": {"url_q" : "https://skipthedrive.com/?s=","active":0}, #status code 403
-    "workingnomads": {"url_q" : "https://workingnomads.com/jobs?tag=", "active":1},
-    "vagas": {"url_q" : "https://vagas.com.br/vagas-", "active":0}, #endpoint diferente do padrão
+sources = {
+    "engenha": {"url" : "https://engenha.com/vagas?q=data+analyst%7Csenior", "active":1},
+    "workingnomads": {"url" : "https://www.workingnomads.com/jobsapi/_search", "active":1},
+}
+
+queries = {
+    # "data engineer",
+    # "data analytics",
+    "analytics engineer",
+    # "data architect",
+    # "big data engineer",
+    # "cloud data engineer",
+    # "database administrator",
+    # "engenheiro de dados",
+    # "business intelligence analyst",
+    # "analista de bi",
+    # "bi developer",
+    # "product data analyst",
+    # "marketing data analyst",
+    # "analista de dados",
+    # "reporting analyst",
+    # "data scientist",
+    # "cientista de dados",
+    # "machine learning engineer",
+    # "mlops engineer",
+    # "ai engineer",
+    # "data governance analyst",
+    # "data quality engineer"
 }
